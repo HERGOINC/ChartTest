b16ccc7d834e6479b640342fe593daf9121b612b
HERGOINC <sales@hergo.jp>
2023-05-20 22:19:03 +0900

    Initial Commit

---
+++ ChartTest/ChartTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartTestApp.swift
+//  ChartTest
+//
+//  Created by Daisuke Yanagi on 2023/05/20.
+//
+
+import SwiftUI
+
+@main
+struct ChartTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
